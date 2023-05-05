3d7359ca7801f664bd83c617a78610a09f65ca23
askepen <aske@vhg26.dk>
2023-05-05 16:37:04 +0200

    Scuffed zoom

--- main.py
+++ main.py
@@ -1,99 +1,133 @@
 from flask import Flask, jsonify, render_template, request
 import random
+from dataclasses import dataclass
+
+import flask
 
 MAX_NODES = 10000
 
 app = Flask(__name__)
 
-nodes = {}
-edges = {}
+
+@dataclass(order=True, frozen=True)
+class NodeId:
+    id: int
 
 
-def new_id() -> int:
+@dataclass(eq=True, order=True)
+class Point:
+    x: int
+    y: int
+
+
+@dataclass(eq=True, order=True)
+class Node:
+    id: NodeId
+    position: Point
+
+
+@dataclass(eq=True, order=True)
+class Edge:
+    start_node_id: NodeId
+    end_node_id: NodeId
+    start_position: Point
+    end_position: Point
+
+
+@dataclass(eq=True, order=True)
+class Graph:
+    nodes: dict[NodeId, Node]
+    edges: dict[tuple[NodeId, NodeId], Edge]
+
+
+nodes: dict[NodeId, Node] = {}
+edges: dict[tuple[NodeId, NodeId], Edge] = {}
+
+
+def new_id() -> NodeId:
     id = random.randint(0, MAX_NODES)
     while id in nodes.keys():
         id = random.randint(0, MAX_NODES)
-    return id
+    return NodeId(id)
 
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index() -> flask.Response:
     global nodes, edges
 
-    return render_template("index.html", nodes=nodes.values(), edges=edges.values())
+    return flask.make_response(
+        render_template("index.html", nodes=nodes.values(), edges=edges.values())
+    )
 
 
 @app.route("/create", methods=["POST"])
-def create_node():
+def create_node() -> flask.Response:
     global nodes
 
     x = int(request.form["x"])
     y = int(request.form["y"])
     node_id = new_id()
-    node = {"id": node_id, "x": x, "y": y}
+    node = Node(node_id, Point(x, y))
     nodes[node_id] = node
+    print(node)
     return jsonify(node)
 
 
 @app.route("/delete", methods=["POST"])
-def delete_node():
+def delete_node() -> flask.Response:
     global nodes, edges
 
-    node_id = int(request.form["node_id"])
+    node_id = NodeId(int(request.form["node_id"]))
     nodes.pop(node_id)
     # nodes = [node for node in nodes if node["id"] != node_id]
     for other_node in nodes.keys():
         edges.pop((node_id, other_node), None)
         edges.pop((other_node, node_id), None)
-    return "", 200
+    return flask.make_response("", 200)
 
 
 @app.route("/move", methods=["POST"])
-def move_node():
+def move_node() -> flask.Response:
     global nodes
 
-    node_id = int(request.form["node_id"])
+    node_id = NodeId(int(request.form["node_id"]))
     x = int(request.form["x"])
     y = int(request.form["y"])
-    node = {"id": node_id, "x": x, "y": y}
+    node = Node(node_id, Point(x, y))
     nodes[node_id] = node
     for (edge_start_id, edge_end_id), edge in edges.items():
         if node_id == edge_start_id:
-            edge["x1"] = x
-            edge["y1"] = y
+            edge.start_position = Point(x, y)
         elif node_id == edge_end_id:
-            edge["x2"] = x
-            edge["y2"] = y
-    return "", 204
+            edge.end_position = Point(x, y)
+    return flask.make_response("", 204)
 
 
 @app.route("/connect", methods=["POST"])
-def connect():
+def connect() -> flask.Response:
     global edges
 
-    start_node_id = int(request.form["start_node_id"])
-    end_node_id = int(request.form["end_node_id"])
-    edge = {
-        "start_node_id": start_node_id,
-        "end_node_id": end_node_id,
-        "x1": nodes[start_node_id]["x"],
-        "y1": nodes[start_node_id]["y"],
-        "x2": nodes[end_node_id]["x"],
-        "y2": nodes[end_node_id]["y"],
-    }
+    start_node_id = NodeId(int(request.form["start_node_id"]))
+    end_node_id = NodeId(int(request.form["end_node_id"]))
+    edge = Edge(
+        start_node_id,
+        end_node_id,
+        Point(nodes[start_node_id].position.x, nodes[start_node_id].position.y),
+        Point(nodes[end_node_id].position.x, nodes[end_node_id].position.y),
+    )
     edges[(start_node_id, end_node_id)] = edge
     return jsonify(edge)
 
 
 @app.route("/nodes", methods=["GET"])
-def get_nodes():
+def get_nodes() -> flask.Response:
     global nodes
 
     return jsonify(list(nodes.values()))
 
 
 @app.route("/edges", methods=["GET"])
-def get_edges():
+def get_edges() -> flask.Response:
     global edges
 
     return jsonify(list(edges.values()))
--- script.ts
+++ script.ts
@@ -3,6 +3,7 @@ let offset_x = 0;
 let offset_y = 0;
 let global_offset_x = 0;
 let global_offset_y = 0;
+let global_zoom = 1;
 const nodeSize = 50;
 
 enum State {
@@ -12,7 +13,43 @@ enum State {
     Delete = "delete",
 }
 
+enum DragState {
+    DraggingNode = "dragging_node",
+    DraggingGraph = "dragging_graph",
+    None = "none",
+}
+
+type GraphNodeId = {
+    id: string;
+};
+
+type Position = {
+    x: number;
+    y: number;
+};
+
+type GraphNode = {
+    id: GraphNodeId;
+    position: Position;
+};
+
+
+type Edge = {
+    start_node_id: GraphNodeId;
+    end_node_id: GraphNodeId;
+    start_position: Position;
+    end_position: Position;
+};
+
 let state: State = State.Drag;
+let dragState: DragState = DragState.None;
+
+
+var canvas = document.getElementById("canvas");
+if (!canvas || !(canvas instanceof SVGSVGElement)) {
+    throw new Error("canvas not found");
+}
+
 
 function changeState(newState: State) {
     state = newState;
@@ -20,42 +57,115 @@ function changeState(newState: State) {
     console.log(state);
 }
 
+function transformCoords(x: number, y: number, screenToGraph: boolean): SVGPoint {
+    const svg = document.getElementById("canvas") as SVGSVGElement | null;
+    const graph = document.getElementById("graph") as SVGGElement | null;
+    if (!graph || !svg) {
+        throw new Error("Could not find SVGSVGElement with ID 'graph'");
+    }
+
+    const pt = svg.createSVGPoint();
+    pt.x = x;
+    pt.y = y;
+
+    const screenCTM = graph.getScreenCTM();
+    if (screenCTM) {
+        return pt.matrixTransform(screenToGraph ? screenCTM.inverse() : screenCTM);
+    }
+
+    return pt;
+}
+
+function graphToScreenCoords(x: number, y: number): SVGPoint {
+    return transformCoords(x, y, false)
+}
+
+function screenToGraphCoords(x: number, y: number): SVGPoint {
+    return transformCoords(x, y, true)
+}
+
+
 function dragStart(event: MouseEvent) {
     if (event.target instanceof SVGRectElement) {
         selected_node = event.target;
     } else if (event.target instanceof SVGTextElement) {
         selected_node = event.target.previousElementSibling as SVGRectElement;
+    } else if (event.target instanceof SVGSVGElement) {
+        selected_node = null;
+        offset_x = event.clientX;
+        offset_y = event.clientY;
+        dragState = DragState.DraggingGraph;
+        return;
     } else {
         return;
     }
-    offset_x = event.clientX - parseInt(selected_node.getAttribute("x")!);
-    offset_y = event.clientY - parseInt(selected_node.getAttribute("y")!);
+    const point = graphToScreenCoords(
+        parseInt(selected_node.getAttribute("x")!),
+        parseInt(selected_node.getAttribute("y")!)
+    );
+    offset_x = event.clientX - point.x;
+    offset_y = event.clientY - point.y;
+    dragState = DragState.DraggingNode;
 }
 
+function updateGraphTransform() {
+    const graph = document.getElementById("graph")!;
+    if (!(canvas instanceof SVGSVGElement) || !(graph instanceof SVGGraphicsElement)) {
+        throw new Error("canvas or graph not found");
+    }
+
+    const scale = canvas.createSVGTransform();
+    scale.setScale(global_zoom, global_zoom);
+
+    // Translate to the current offset
+    const translateOffset = canvas.createSVGTransform();
+    translateOffset.setTranslate(global_offset_x / global_zoom, global_offset_y / global_zoom);
+
+    const transformList = graph.transform.baseVal;
+    transformList.clear();
+    transformList.appendItem(scale);
+    transformList.appendItem(translateOffset);
+}
+
+
+
 function drag(event: MouseEvent) {
-    if (selected_node) {
-        const x = event.clientX - offset_x;
-        const y = event.clientY - offset_y;
-        selected_node.setAttribute("x", x.toString());
-        selected_node.setAttribute("y", y.toString());
+    if (dragState === DragState.DraggingNode && selected_node) {
+        const point = screenToGraphCoords(event.clientX - offset_x, event.clientY - offset_y);
+        selected_node.setAttribute("x", point.x.toString());
+        selected_node.setAttribute("y", point.y.toString());
         const node_id = selected_node.getAttribute("data-id")!;
-        updateEdges(node_id, x, y);
+        updateEdges(node_id, point.x, point.y);
         const label = selected_node.nextElementSibling as SVGTextElement;
-        label.setAttribute("x", x.toString());
-        label.setAttribute("y", y.toString());
+        label.setAttribute("x", point.x.toString());
+        label.setAttribute("y", point.y.toString());
+    } else if (dragState === DragState.DraggingGraph) {
+        global_offset_x += event.clientX - offset_x;
+        global_offset_y += event.clientY - offset_y;
+        offset_x = event.clientX;
+        offset_y = event.clientY;
+        updateGraphTransform();
     }
 }
 
 function dragEnd(_event: MouseEvent) {
-    if (!selected_node) return;
-    var node_id = selected_node!.getAttribute("data-id")!;
-    var x = parseInt(selected_node!.getAttribute("x")!);
-    var y = parseInt(selected_node!.getAttribute("y")!);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/move");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.send(`node_id=${node_id}&x=${x}&y=${y}`);
-    selected_node = null;
+    if (dragState === DragState.DraggingNode) {
+        var node_id = selected_node!.getAttribute("data-id")!;
+        var x = parseInt(selected_node!.getAttribute("x")!);
+        var y = parseInt(selected_node!.getAttribute("y")!);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/move");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send(`node_id=${node_id}&x=${x}&y=${y}`);
+        selected_node = null;
+    }
+    dragState = DragState.None;
+}
+
+function handleScroll(event: WheelEvent) {
+    global_zoom = Math.min(Math.max(0.125, global_zoom + event.deltaY * -0.01), 4);
+    updateGraphTransform();
+    event.preventDefault();
 }
 
 
@@ -92,8 +202,10 @@ function connectClick(event: MouseEvent) {
 }
 
 function createClick(event: MouseEvent) {
-    var x = event.clientX;
-    var y = event.clientY;
+    var x = event.x;
+    var y = event.y;
+    console.log(event);
+
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "/create");
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -130,33 +242,28 @@ function deleteClick(event: MouseEvent) {
     xhr.send(`node_id=${node_id}`);
 }
 
-function addEdgeToCanvas(edge: { id: string; start_node_id: string; end_node_id: string; x1: string; y1: string; x2: string; y2: string; }) {
+function addEdgeToCanvas(edge: Edge) {
     var edges = document.getElementById("edges");
-    if (!edges) {
-        throw new Error("edges not found");
-    }
+    if (!edges) throw new Error("edges not found");
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     line.setAttribute("class", "edge");
-    line.setAttribute("x1", edge.x1);
-    line.setAttribute("y1", edge.y1);
-    line.setAttribute("x2", edge.x2);
-    line.setAttribute("y2", edge.y2);
+    line.setAttribute("x1", edge.start_position.x.toString());
+    line.setAttribute("y1", edge.start_position.y.toString());
+    line.setAttribute("x2", edge.end_position.x.toString());
+    line.setAttribute("y2", edge.end_position.y.toString());
+    line.setAttribute("data-start-node-id", edge.start_node_id.id);
+    line.setAttribute("data-end-node-id", edge.end_node_id.id);
     line.setAttribute("stroke", "black");
-    line.setAttribute("data-id", edge.id);
-    line.setAttribute("data-start-node-id", edge.start_node_id);
-    line.setAttribute("data-end-node-id", edge.end_node_id);
     edges.appendChild(line);
 }
 
-function addNodeToCanvas(node: { id: string; x: number; y: number; }) {
+function addNodeToCanvas(node: GraphNode) {
     var nodes = document.getElementById("nodes");
-    if (!nodes) {
-        throw new Error("nodes not found");
-    }
+    if (!nodes) throw new Error("nodes not found");
     var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
     rect.setAttribute("class", "node");
-    rect.setAttribute("x", (node.x).toString());
-    rect.setAttribute("y", (node.y).toString());
+    rect.setAttribute("x", (node.position.x).toString());
+    rect.setAttribute("y", (node.position.y).toString());
     rect.setAttribute("width", nodeSize.toString());
     rect.setAttribute("height", nodeSize.toString());
     rect.setAttribute("transform", `translate(${-nodeSize / 2}, ${-nodeSize / 2})`);
@@ -164,17 +271,17 @@ function addNodeToCanvas(node: { id: string; x: number; y: number; }) {
     rect.setAttribute("fill", "#eee");
     rect.setAttribute("stroke", "#ccc");
     rect.setAttribute("stroke-width", "1");
-    rect.setAttribute("data-id", node.id);
+    rect.setAttribute("data-id", node.id.id);
     nodes.appendChild(rect);
     var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    text.setAttribute("x", (node.x).toString());
-    text.setAttribute("y", (node.y).toString());
-    text.setAttribute("data-id", node.id);
+    text.setAttribute("x", (node.position.x).toString());
+    text.setAttribute("y", (node.position.y).toString());
+    text.setAttribute("data-id", node.id.id);
     text.setAttribute("font-size", "20");
     text.setAttribute("font-family", "Arial");
     text.setAttribute("text-anchor", "middle");
     text.setAttribute("dominant-baseline", "middle");
-    text.textContent = node.id;
+    text.textContent = node.id.id;
     nodes.appendChild(text);
 }
 
@@ -191,7 +298,7 @@ function removeNodeFromCanvas(node_id: string) {
         nodes?.removeChild(element);
     });
     var edges = document.getElementsByClassName("edge");
-    for (let i = 0; i < edges.length; i++) {
+    for (var i = edges.length - 1; i >= 0; i--) {
         const start_node_id = edges[i].getAttribute("data-start-node-id");
         const end_node_id = edges[i].getAttribute("data-end-node-id");
         if (start_node_id === node_id || end_node_id === node_id) {
@@ -202,7 +309,7 @@ function removeNodeFromCanvas(node_id: string) {
 
 function updateEdges(node_id: string, x: number, y: number) {
     const edges = document.getElementsByClassName("edge");
-    for (let i = 0; i < edges.length; i++) {
+    for (var i = edges.length - 1; i >= 0; i--) {
         const start_node_id = edges[i].getAttribute("data-start-node-id");
         const end_node_id = edges[i].getAttribute("data-end-node-id");
         if (start_node_id === node_id) {
@@ -244,11 +351,6 @@ function renderEdges() {
     xhr.send();
 }
 
-
-var canvas = document.getElementById("canvas");
-if (!canvas) {
-    throw new Error("canvas not found");
-}
 
 canvas.addEventListener("mousedown", (event: MouseEvent) => {
     switch (state) {
@@ -294,12 +396,24 @@ canvas.addEventListener("mouseup", (event: MouseEvent) => {
     }
 }
 );
+canvas.addEventListener("mouseleave", (event: MouseEvent) => {
+    switch (state) {
+        case State.Drag:
+            dragEnd(event);
+            break;
+        case State.Create:
+            break;
+        case State.Connect:
+            break;
+        case State.Delete:
+            break;
+    }
+});
+canvas.addEventListener("wheel", (event: WheelEvent) => { handleScroll(event) });
 
 document.getElementById('btn-create')?.addEventListener('click', () => { changeState(State.Create) });
-document.getElementById('btn-connect')?.addEventListener('click', () => { changeState(State.Connect); });
-document.getElementById('btn-delete')?.addEventListener('click', () => { changeState(State.Delete); });
-
-
+document.getElementById('btn-connect')?.addEventListener('click', () => { changeState(State.Connect) });
+document.getElementById('btn-delete')?.addEventListener('click', () => { changeState(State.Delete) });
 
 renderNodes();
 renderEdges();
